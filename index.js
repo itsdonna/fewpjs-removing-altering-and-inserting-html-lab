6cf2a821744a38b64fa8be3afa5b0bb8ae77ffbb
Donna Torres <todonnatorres@icloud.com>
2021-01-29 22:02:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('main') //parent
+main.remove() //removing the element itself
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Donna is the champion"
